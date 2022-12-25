155539525c3f140a991f192c2335e736ef9a3437
Denis <demid200479@gmail.com>
2022-12-25 16:03:24 +0300

    домашка2

---
+++ Seminar2/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+int CutNumber(int number)
+{
+    int sot = number / 100;
+    int edi = number % 10;
+    int result = sot*10 + edi; 
+    return result;
+}
+
+
+int randNumber = new Random().Next(100, 1000);
+int NewNumber = CutNumber(randNumber);
+Console.WriteLine($"new version of a number {randNumber} is {NewNumber}" );
+*/
