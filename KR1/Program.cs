3fccb8f8807e5f97b4637b2b67ee8e9f3318c229
Denis <demid200479@gmail.com>
2023-03-06 21:37:46 +0300

    first idea

--- KR1/Program.cs
+++ KR1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//решение задачи
+//пункт 1) метод создающий массив
+string[] CreateArray()
+{
+    Console.Write("Кол-во элементов: ");
+    int countString = Convert.ToInt32(Console.ReadLine());
+    string[] newArray = new string[countString];
+     for (int i = 0; i < countString; i++)
+        {
+            Console.Write($"Введите элемент №{i + 1}: ");
+            newArray[i] += Console.ReadLine();
+        }
+    return newArray;
+}
+
